d1ae35374596f69c99b1731343214af749110fb8
megh-m <125548504+megh-m@users.noreply.github.com>
2025-03-10 15:15:02 +0530

    Update train.py. Set default hyperparameter values based on sweeps

--- train.py
+++ train.py
@@ -7,20 +7,20 @@ def parse_args():
     parser = argparse.ArgumentParser(description='To train network')
     
     # Wandb parameters as given in assignment statement
-    parser.add_argument('-wp', '--wandb_project', type=str, default='myprojectname',
+    parser.add_argument('-wp', '--wandb_project', type=str, default='DA6401_Assignment-1',
                         help='Project name used to track experiments in Weights & Biases dashboard')
-    parser.add_argument('-we', '--wandb_entity', type=str, default='myname',
+    parser.add_argument('-we', '--wandb_entity', type=str, default='megh_m-iit-madras',
                         help='Wandb Entity used to track experiments in the Weights & Biases dashboard.')
     
     # Dataset
     parser.add_argument('-d', '--dataset', type=str, default='fashion_mnist', 
                         choices=["mnist", "fashion_mnist"],
-                        help='Dataset to use for training')
+                        help='Dataset to use for model')
     
     # Training parameters
-    parser.add_argument('-e', '--epochs', type=int, default=1,
+    parser.add_argument('-e', '--epochs', type=int, default=20,
                         help='Number of epochs to train neural network.')
-    parser.add_argument('-b', '--batch_size', type=int, default=4,
+    parser.add_argument('-b', '--batch_size', type=int, default=32,
                         help='Batch size used to train neural network.')
     
     # Loss function
@@ -29,22 +29,22 @@ def parse_args():
                         help='Loss function used to train neural network.')
     
     # Optimizer
-    parser.add_argument('-o', '--optimizer', type=str, default='sgd',
+    parser.add_argument('-o', '--optimizer', type=str, default='adam',
                         choices=["sgd", "momentum", "nesterov", "rmsprop", "adam", "nadam"],
                         help='Optimizer used to train neural network.')
     
     # Optimizer parameters
-    parser.add_argument('-lr', '--learning_rate', type=float, default=0.1,
+    parser.add_argument('-lr', '--learning_rate', type=float, default=0.01,
                         help='Learning rate used to optimize model parameters')
-    parser.add_argument('-m', '--momentum', type=float, default=0.5,
+    parser.add_argument('-m', '--momentum', type=float, default=0.9,
                         help='Momentum used by momentum and nag optimizers.')
-    parser.add_argument('-beta', '--beta', type=float, default=0.5,
+    parser.add_argument('-beta', '--beta', type=float, default=0.9,
                         help='Beta used by rmsprop optimizer')
-    parser.add_argument('-beta1', '--beta1', type=float, default=0.5,
+    parser.add_argument('-beta1', '--beta1', type=float, default=0.9,
                         help='Beta1 used by adam and nadam optimizers.')
-    parser.add_argument('-beta2', '--beta2', type=float, default=0.5,
+    parser.add_argument('-beta2', '--beta2', type=float, default=0.99,
                         help='Beta2 used by adam and nadam optimizers.')
-    parser.add_argument('-eps', '--epsilon', type=float, default=0.000001,
+    parser.add_argument('-eps', '--epsilon', type=float, default=0.00000001,
                         help='Epsilon used by optimizers.')
     parser.add_argument('-w_d', '--weight_decay', type=float, default=0.0,
                         help='Weight decay used by optimizers.')
@@ -53,9 +53,9 @@ def parse_args():
     parser.add_argument('-w_i', '--weight_init', type=str, default='random',
                         choices=["random", "Xavier"],
                         help='Weight initialization method.')
-    parser.add_argument('-nhl', '--num_layers', type=int, default=1,
+    parser.add_argument('-nhl', '--num_layers', type=int, default=3,
                         help='Number of hidden layers used in feedforward neural network.')
-    parser.add_argument('-sz', '--hidden_size', type=int, default=4,
+    parser.add_argument('-sz', '--hidden_size', type=int, default=64,
                         help='Number of hidden neurons in a feedforward layer.')
     parser.add_argument('-a', '--activation', type=str, default='sigmoid',
                         choices=["identity", "sigmoid", "tanh", "ReLU"],
@@ -113,7 +113,7 @@ def parse_args():
 
     # Training loop
     for epoch in range(args.epochs):
-        # Shuffle the training data
+        # Shuffling the training data every epoch
         indices = np.random.permutation(len(X_train))
         X_train_shuffled = X_train[indices]
         y_train_shuffled = y_train[indices]
@@ -142,7 +142,7 @@ def parse_args():
             "train_loss": train_loss,
             "test_loss": test_loss,
             "train_accuracy": train_accuracy,
-            "test_accuracy": test_accuracy
+            "validation_accuracy": test_accuracy
         })
 
         print(f"Epoch {epoch+1}/{args.epochs}, "
@@ -159,7 +159,9 @@ def parse_args():
     # Log final metrics
     wandb.log({
         "final_train_accuracy": final_train_accuracy,
-        "final_test_accuracy": final_test_accuracy
+        "final_validation_accuracy": final_test_accuracy
+        "final_train_loss": train_loss,
+        "final_test_loss": test_loss
     })
 
     wandb.finish()
